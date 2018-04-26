40f2e5821df25cee1f48aeb3909de9aca9e6d224
binauralbeat <binauralbeat001@gmail.com>
2018-04-26 10:16:37 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+//fnal output to dome
+const output = document.querySelector("output")
+
+//fragment to hold all the stuff so we inject into the dom once
+const fragment = document.createDocumentFragment()
+
+// create factory function for comonents
+//create input component
+const inputFieldFactory = (classList, defaultPlaceholderText) => {
+    const inputField = document.createElement("input")
+    inputField.setAttribute("type", type)
+    inputField.classList = classList
+    inputField.type = type
+    inputField.placeholder = defaultPlaceholderText
+    return inputField
+
+}
+
+const createInputField =  inputFieldFactory("input--text", "Enter card text here", "text")
+
+createInputField.addEventListener()
+
+//create button component
+
+const buttonFactory = (classList, textContent) => {
+    const theButton = document.createElement("buton")
+    inputField.classList = classList
+    inputField.placeholder = defaultPlaceholderText
+    return theButton
+
+}
+
+const createCardButton = buttonFactory("button--submit", "Create Card")
+
+createCardButton.addEventListener("click", function (){
+
+
+        const userEntry = cardTextInput.textContent
+
+
+})
+
+fragment.appendChild()
+fragment.appendChild()
+
+
+
+/*attach event listener to button
+    1. get value of input field
+    2. create card component with text inside 
+*/
+
+
+//create card component
+
+output.appendChild(fragment)
+
+
